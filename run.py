d098189ddeb42ccb62f7b2355af44e30f4d9479e
Kat24C <kcsurvey47@gmail.com>
2022-07-06 13:51:33 +0000

    Fix errors in program

--- run.py
+++ run.py
@@ -29,15 +29,81 @@ def game_choice():
     comp_score = 0
     while True:
         try:
-            user_choice = int(input("Choose a number between 1 and 3: \n"))
-            if user_choice not in range(1, 3):
+            user_choice = int(input("Choose a number between 1 and 3: "))
+            user_guess = int(input("Guess the computers number: "))
+            if user_choice not in range(1, 4):
                 print("Please enter a number between 1 and 3")
                 user_choice = int(input("Choose a number between 1 and 3: \n"))
-            else: 
+            elif user_guess not in range(1, 4):
+                print("Please enter a number between 1 and 3")
+                user_guess = int(input("Guess the computers number: "))
+            else:
                 computer_guess = random.randint(1, 3)
-                print(f"The computer chose: {computer_guess}\n")
+                print(f"The computer chose: {computer_guess}")
+                computer_choice = random.randint(1, 3)
+                print(f"The computer guessed: {computer_choice}\n")
+                game_score(user_guess, user_choice, computer_guess, computer_choice, player_score, comp_score)
         except ValueError:
-            print("Please enter a valid number")       
-        
+            print("Please enter a valid number")
+  
+     
+def game_score(user_guess, user_choice, computer_guess, computer_choice, player_score, comp_score):
+    """
+    compare score
+    """      
+    if user_guess == computer_choice:
+        user_score = user_guess + computer_choice
+        print(f"Well done, You got {user_score}")
+        player_score = player_score + user_score
+        print(f"Your total is {player_score}\n")
+        finalgame(player_score, comp_score)
+
+    else:
+        print("Oh dear, try again you got 0")
+        print(f"Your total is {player_score}")
+        finalgame(player_score, comp_score)
+
+    if user_choice == computer_guess:
+        computer_score = user_choice + computer_guess
+        print(f"The computer got {computer_score}")
+        comp_score = comp_score + computer_score
+        print(f"The computer's total is {comp_score}\n")
+        finalgame(player_score, comp_score)
+
+    else:
+        print("The computer got 0")
+        print(f"The computer's total is {comp_score}\n")
+        finalgame(player_score, comp_score)
+
+
+def finalgame(player_score, comp_score):
+    """
+    This will decide if the game continues
+    """
+    if player_score >= 12:
+        print("Congratulations you win :)")
+        play_again()
+    elif comp_score >= 12:
+        print("Sorry the computer won :(")
+        play_again()
+    else:
+        return True
+
+
+def play_again():
+    """
+    Asks the user if they would like to restart the game.
+    """
+    play_more = input("Would you like to play again? Yes/No: \n").lower()
+    if play_more == "yes":
+        print("Fantastic lets play again.")
+        print()
+        game_choice()
+    elif play_more == "no":
+        quit()
+    else:
+        print("Invalid option")
+        play_again()
+
 
 user_details()
